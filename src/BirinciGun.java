279967b535ddc943858d750c515aa5fb86038390
Fatih Ozbahceci <ozbahcecifatih@gmail.com>
2020-11-17 21:56:19 +0100

    localden mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -59,5 +59,6 @@ cls   -> cmd ekranını temizliyor.
 	      System.out.println("21:05-local");
 	      System.out.println("21:24-github");
 	      System.out.println("burada conflict beklemiyorum");
+	      System.out.println("localden yeni satir");
 }
 }
